96e0dce8b6998e1cf46dcd6be192b76cc7548a63
nikolardo <nikolardoflamingo@gmail.com>
2019-03-03 17:12:02 -0500

    Added documentation

--- workingScript.py
+++ workingScript.py
@@ -15,16 +15,24 @@ materials.clear(update_data=True)
 for mat in matsToKeep:
     materials.append(mat)
 
+#vertexColor is an array of vertex colors
 vertexColor = data.vertex_colors[0].data
 i = 0
+# for each face of the polygon
 for f in obj.data.polygons:
+    # copy the existing material
     mat = materials[f.material_index].copy()me
-    
+    # give it an appropriate name
     mat.name = ('NikMat' + str(f.index))
     col = 0
+    # for each vertex
+    # color = vertexColor[i]
     for idx in f.loop_indices:
         col = vertexColor[i].color
         i += 1
+    # set that color to the specular color
     mat.specular_color = col
+    # add it as a material
     obj.data.materials.append(mat)
+    # set the current face to use that material
     f.material_index = len(obj.data.materials)-1
